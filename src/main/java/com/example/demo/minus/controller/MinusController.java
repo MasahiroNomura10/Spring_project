686f67b27a2d23360a1de9387dd2e283edbb42e7
Owner-PC\Owner <m-nomura@seassist.co.jp>
2024-11-22 19:12:24 +0900

    test

--- src/main/java/com/example/demo/minus/controller/MinusController.java
+++ src/main/java/com/example/demo/minus/controller/MinusController.java
@@ -1,13 +1,7 @@
 package com.example.demo.minus.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.example.demo.minus.service.MinusService;
-
-
 
 
 
@@ -15,13 +9,6 @@ import com.example.demo.minus.service.MinusService;
 @Controller
 public class MinusController {
 
-	MinusService minus = new MinusService();
-	
-	public MinusController( MinusService minusService ) {
-		
-		this.minus = minusService;
-	}
-	
 	
 	//スタート画面
 	@GetMapping( "/spring_03")
@@ -30,27 +17,50 @@ public class MinusController {
 	}
 	
 	
-	//計算後画面
-	@GetMapping("calculationAnswer")
-	public String calculationAnswer( @RequestParam( "num1" )int num1, @RequestParam( "num2" )int num2, Model model ) {
-				
-		//String result = "";
-		String n1 ="";
-		
-
-		n1 = String.valueOf( num1 );
-		
-		model.addAttribute("num1", n1);
-		
-//		result = String.valueOf( minus.minus( num1, num2 ) );
-//		
-//		
-//		model.addAttribute( "result", result );
-		
-		
-		return "minus";
-	}
-	
 	
 	
 }
+
+
+//@Controller
+//public class MinusController {
+//
+//	MinusService minus = new MinusService();
+//	
+//	public MinusController( MinusService minusService ) {
+//		
+//		this.minus = minusService;
+//	}
+//	
+//	
+//	//スタート画面
+//	@GetMapping( "/spring_03")
+//	public String spring_03(){
+//		return "minus";
+//	}
+//	
+//	
+//	//計算後画面
+//	@GetMapping("calculationAnswer")
+//	public String calculationAnswer( @RequestParam( "num1" )int num1, @RequestParam( "num2" )int num2, Model model ) {
+//				
+//		//String result = "";
+//		String n1 ="";
+//		
+//
+//		n1 = String.valueOf( num1 );
+//		
+//		model.addAttribute("num1", n1);
+//		
+////		result = String.valueOf( minus.minus( num1, num2 ) );
+////		
+////		
+////		model.addAttribute( "result", result );
+//		
+//		
+//		return "minus";
+//	}
+//	
+//	
+//	
+//}
--- src/main/java/com/example/demo/minus/service/MinusService.java
+++ src/main/java/com/example/demo/minus/service/MinusService.java
@@ -7,14 +7,39 @@ import org.springframework.stereotype.Service;
 
 
 
-
-
 @Service
 public class MinusService {
+
 	
-		
-	public int minus( int num1, int num2 ) {
-		
-		return num1 - num2;
+	private String val1;
+	private String val2;
+
+	public String getVal1() {
+		return val1;
 	}
+
+	public void setVal1(String val1) {
+		this.val1 = val1;
+	}
+
+	public String getVal2() {
+		return val2;
+	}
+
+
+	public void setVal2(String val2) {
+		this.val2 = val2;
+	}
+
 }
+
+
+//@Service
+//public class MinusService {
+//	
+//		
+//	public int minus( int num1, int num2 ) {
+//		
+//		return num1 - num2;
+//	}
+//}
